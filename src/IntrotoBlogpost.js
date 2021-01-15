924a381215b48d25125486f341ffa82a27deb9dc
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-01-15 03:03:23 -0700

    Simplify CSS import

--- src/IntrotoBlogpost.js
+++ src/IntrotoBlogpost.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Nav } from 'react-bootstrap'
-import BlogpostStyles from './css/BlogpostStyles.css'
+import './css/BlogpostStyles.css'
 import Me from './images/Me.jpg'
 
 
