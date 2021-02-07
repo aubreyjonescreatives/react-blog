88cb28cb5d243f41a621484340c1d34f743bc328
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-02-06 20:48:13 -0700

    Add unit testing blogpost

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import IntrotoBlogpost from './IntrotoBlogpost'
 import ResearchandLearning from './ResearchandLearning'
 import Git from './Git'
+import UnitTesting from './UnitTesting'
 import TopNavigation from './TopNavigation'
 import Header from './Header'
 import HeroImage from './HeroImage'
@@ -13,6 +14,7 @@ function App() {
     <Header/>
     <TopNavigation/>
     <HeroImage/>
+    <UnitTesting/>
     <Git/>
     <ResearchandLearning/>
     <IntrotoBlogpost/>
---
+++ src/UnitTesting.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './css/BlogpostStyles.css'
+import GitImage from './images/gitimage.PNG'
+
+
+
+function UnitTesting() {
+    return (
+     
+     <div class="main-1" id="unittesting">
+    <h1>UNIT TESTING</h1>
+     <p><figure className="gitImage"> <img src={GitImage} alt="Unit Testing"></img></figure>
+     What is a unit? 
+    
+    
+     What is unit testing? 
+
+    
+      </p>
+      <p>
+    What is test driven development? 
+
+
+        </p>
+        <p>
+     Why would you practice test driven development? 
+
+     What are the advantages and disadvantages of test driven development? 
+
+     </p>
+     </div>
+    );
+  }
+  
+  export default UnitTesting;
+  
