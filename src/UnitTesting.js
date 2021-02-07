8a4d2f388aa066e0badd4bdd3efe38c95fa0e61d
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-02-06 20:59:22 -0700

    What is a unit explained?

--- src/UnitTesting.js
+++ src/UnitTesting.js
@@ -10,10 +10,9 @@ function UnitTesting() {
      <div class="main-1" id="unittesting">
     <h1>UNIT TESTING</h1>
      <p><figure className="gitImage"> <img src={GitImage} alt="Unit Testing"></img></figure>
-     What is a unit? 
-    
-    
-     What is unit testing? I find it easy to understand unit testing by taking units of code and testing their 
+     What is a unit? Think about all of the letters and numbers combined together to create a 
+     programming language. A unit is the smallest isolated logical piece of code. For example, it could be 
+     a variable i for an integer inside a for loop. So what is unit testing? I find it easy to understand unit testing by taking units of code and testing their 
      use of that code to see if it works. We can test a unit as simple as a single variable integer or several 
      snippets of code at a time.   
 
