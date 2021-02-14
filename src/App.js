50f82b84a0f2d8f225b79c7fef9a9cb800b1d655
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-02-13 23:30:37 -0700

    fixed header

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
     <div className="App">
     <Header/>
     <TopNavigation/>
-    <HeroImage/>
     <TypeScript/>
     <UnitTesting/>
     <Git/>
--- src/TypeScript.js
+++ src/TypeScript.js
@@ -6,8 +6,9 @@ import typeScriptImage from './images/typescript.PNG'
 
 function TypeScript() {
     return (
-     
+
      <div class="main-1" id="typescript">
+       <div className="EmptyTop"></div>
     <h1>TYPESCRIPT</h1>
      <p><figure className="tsImage"> <img src={typeScriptImage} alt="TypeScript"></img></figure>
      What comes to mind when you think about the TypeScript programming language? JavaScript? If you read up on the 
