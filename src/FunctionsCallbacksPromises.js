af3f82a4ccf2b4331f5df78e98ad6f71e2041b36
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-03-06 15:24:01 -0700

    add function and callback info

--- src/FunctionsCallbacksPromises.js
+++ src/FunctionsCallbacksPromises.js
@@ -1,54 +1,69 @@
 import React from 'react'
 import './css/BlogpostStyles.css'
 import functionsImage from './images/functions.PNG'
+import functionsImage2 from './images/function2.PNG'
+import functionsImage3 from './images/function3.PNG'
+import functionsImage4 from './images/functions4.PNG'
+import callback1 from './images/callback1.PNG'
+import returnImage from './images/return.PNG'
+
+
 
 
 
 function FunctionsCallbacksPromises() {
     return (
 
+
+
+
+
      <div class="main-1" id="functionscallbackspromises">
        <div className="EmptyTop"></div>
     <h1>Functions, Callbacks, and Promises</h1>
      <p>
-     How do you declare a function? You can declare a function by several different methods. It can depend upon the 
-     function's title keyword location. For example, one function can begin with the the syntax: function functionName() 
-     such as in the following image:<p>
-       <figure className="functionImage"><img src={functionsImage} alt="functions"></img></figure></p>
-     and another example can begin with: functionName = function() as shown below: 
-     
-     
-      The syntax is so similar. So how are they different?
-     Let's add a parser to both examples. The first example would pass the parser functionName() first, while the second 
-     method would accept the parser functionName() last. This would effect response times for the program to accept 
-     functions. A third style to declaring a function adds arguments to its parameter. Function parameters are the 
-     first set of parenthesis after the function name. For example, they look like this: 
-     function functionName(argument1, argument2). Function arguments alter the function's process, as it has to 
-     take a look at the arguments throughout the function. 
-     <p>If you want to return a value of a function, you'll have to first define either variables or parameters. You can 
-       return a function within a function by simply stating the syntax: return: var. This allows functions to 
-       process requests. A return request can have the syntax such as return: x + y.   
+     How do you declare a function? Why is it important to declare a function? You can accomplish this in several 
+      different ways. For example, parameters help declare a function as they pass individual arguments. Here is an 
+      example of how to display arguments inside the parameter, which is the first set of parenthesis after an argument title:<p>
      </p>
      <p>
-       To accept a value into a function, let's first explain what a value is. 
+       <figure className="functionImage"><img src={functionsImage} alt="functions"></img></figure></p>
+    <p>
+     You can also declare a function as you assign it to a variable, constant, or let with an arrow, 
+     which is a more up-to-date version for writing JavaScript ES6 functions such as described in this image: 
+     </p>
+    <p> <figure className="functionImage"><img src={functionsImage2} alt="functions"></img></figure></p>
+     <p>And lastly, don't forget about declaring a function within the console.log. You can have an array of 
+       objects inside a const. And then map through the cont after the array: 
+     </p>
+     <p> <figure className="functionImage"><img src={functionsImage3} alt="functions"></img></figure></p>
+    <p>So how do you return a value from the function? Values are anything in JavaScript that has a name. 
+      Values can be anywhere from global or local variables, constants, and lets to arguments. Let's take a 
+      look at the following example of where to place the keyword return inside a function:</p> 
+    <p> <figure className="functionImage"><img src={returnImage} alt="functions"></img></figure></p>
+     <p>
+      You can first return a value within the function, and then either parse it through a parser or the console.  
      </p>
       </p>
       <p>
-      So how can we fix this? Should we forget about frameworks such as React? Harris goes on to say, "Frameworks are 
-      not tools for organizing your code. They are tools for organizing your mind...compilers are the new framework." 
-      Harris therefore created a solution call Svelte. He continues, "It takes your declarative components and then 
-      it turns it into imperative efficient low level code that manipulates the DOM directly." Old Svelte is a lot like 
-      old React when it comes to this.set and this.get. But unfortunately as soon as we introduce .this to both sets of 
-      frameworks, the code connects to all sorts of constraints. Harris created a solution to this (pun intended) as he 
-      found out he could input .change instead of .this. in Svelte, because Svelte is a compiler.   
+      How do you accept a value in a function? While a function itself can be a value, think about constant values. 
+      You can add a constant either globally or locally, and then assign it a value such as in this image: 
         </p>
+        <p><figure className="functionImage"><img src={functionsImage4} alt="functions"></img></figure></p>
         <p>
-       As Harris takes React and Vue projects and shows how Svelte improves them, speed, is one of his results. While it 
-       is fun to showcase Svelte perks, the user experience is most important overall. So how can Svelte accomplish both? 
-       Concurrent React synchronus callbacks only update information when a user stops typing. Svelte synchronus callbacks 
-       update information while a user types. The best user experience presents efficiency. And speed equals efficiency. 
-       It becomes quite clear that Svelte introduces the best user experience as opposed to React and Vue.  
+      The value assigned to a constant in this example has the keyword of numbers, which creates an array of numbers. We 
+      then pass the array into the console. 
      </p>
+     <p>So what about callbacks? Remember how we talked about arguments before? Callbacks come in handy when 
+       we want to display more than one function result. We can display both functions right after one another, as we 
+       call the second function as an argument within the first function parameter. Here is what I mean by this:  
+     </p>
+     <p><figure className="functionImage"><img src={callback1} alt="callback"></img></figure></p>
+     <p>If you take a look at the last line of code in the image above, you can see it creates a callback as the 
+       first function gets passed as an argument in the second function parameter. Callbacks become extremely 
+       useful in asynchronous functions as they sync together to perform different tasks.   
+     </p>
+     <p>While we've explained functions and callbacks, we need to remember promises. What is a promise?</p>
      </div>
 
 
@@ -61,18 +76,16 @@ function FunctionsCallbacksPromises() {
   
 
 
+
+
+
     
 
   }
 
 
 
-
-  function functionTitle(argument1, argument2){
-     
-    return argument1 + argument2
-  }
-  console.log(functionTitle())
+  
 
 
 
